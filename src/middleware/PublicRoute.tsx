aa1233ff904f0bda32dfae01a5b2ad7e07bc8e08
Muhammad Osama <m.osama@sustainablestar.com.sa>
2025-01-24 14:35:36 +0300

    Created PublicRoute middleware. Created Movies section to display all movies. Created Card components to navigate to selected movie details. Created bar graph and pie chart to display key data in dashboard. Enhanced routing in App.tsx

---
+++ src/middleware/PublicRoute.tsx
@@ -0,0 +1,40 @@
+import { RootState } from "@/store";
+import { setData } from "@/store/slices/MovieSlice";
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export const PublicRoute = ({ children }: any) => {
+  const dispatch = useDispatch();
+  const initialDataLoaded = useSelector(
+    (state: RootState) => state?.MoviesSlice?.isLoading
+  );
+
+  const fetchMovies = async () => {
+    const response = await fetch(
+      "https://api.jsonbin.io/v3/b/67935505e41b4d34e47de929/latest",
+      {
+        headers: {
+          "X-Master-Key": import.meta.env.VITE_MASTER_X_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    return data;
+  };
+
+  const { data } = useQuery({
+    queryKey: ["movies"],
+    queryFn: fetchMovies,
+  });
+
+  useEffect(() => {
+    if (data && initialDataLoaded) {
+      dispatch(setData(data.record));
+    }
+  }, [data]);
+
+  return children;
+};
--- src/pages/BrowseMovies.tsx
+++ src/pages/BrowseMovies.tsx
@@ -1,5 +1,60 @@
-import React from "react";
+import { CardComponent } from "@/components/CardComponent";
+import { RootState } from "@/store";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { HiSearch } from "react-icons/hi";
+import { Sidebar } from "@/components/global/Sidebar";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { GoSortDesc } from "react-icons/go";
 
 export const BrowseMovies = () => {
-  return <div>BrowseMovies</div>;
+  const [searchTerm, setSearchTerm] = useState<string>();
+
+  const reduxMovieData = useSelector(
+    (state: RootState) => state?.MoviesSlice?.movies_data
+  );
+
+  return (
+    <div className="sticky ">
+      <Sidebar />
+      <main className="flex flex-col xl:ml-[220px] overflow-y-auto h-[100vh]">
+        {/* search */}
+        <section className="flex w-full justify-between mx-auto my-4 px-8">
+          <div
+            className={`flex items-center  relative h-[50px] border px-2 rounded-md`}
+          >
+            <input
+              type="text"
+              value={searchTerm}
+              placeholder="Search..."
+              // onChange={handleChange}
+              // onKeyDown={handleKeyPress}
+              className={`border-none w-[400px] h-full rounded-md outline-none px-3 flex-1 text-sm`}
+            />
+            <HiSearch className={`text-[#979797] ml-3`} />
+          </div>
+
+          <div className="flex gap-2">
+            <button className="flex items-center border px-2 rounded-md gap-2">
+              <span>Genre</span>
+              <RiArrowDropDownLine size={20} />
+            </button>
+
+            <button className="flex items-center border px-2 rounded-md gap-2">
+              <span>IMDB Rating</span>
+              <GoSortDesc size={20} />
+            </button>
+          </div>
+        </section>
+        {/* cards */}
+        <section className="grid grid-cols-1 md:grid-cols-5 gap-5 place-items-center text-white text-center ">
+          {reduxMovieData?.map((movie, id) => (
+            <div key={id}>
+              <CardComponent data={movie} />
+            </div>
+          ))}
+        </section>
+      </main>
+    </div>
+  );
 };
