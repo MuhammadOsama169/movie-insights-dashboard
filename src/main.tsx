cb90ebc111f09c9595b3e1225ac0d01d80b2e6e6
Muhammad Osama <m.osama@sustainablestar.com.sa>
2025-01-27 11:08:16 +0300

    Update site name and fix type errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import React from "react";
-import AppReduxProvider from "./store/AppReduxProvider.tsx";
+import AppReduxProvider from "./store/AppReduxProvider";
 
 createRoot(document.getElementById("root")!).render(
   <AppReduxProvider>
